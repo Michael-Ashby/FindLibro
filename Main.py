23ef4173026df856841304c22e672d4c874cda50
Michael Ashby <msashby00@gmail.com>
2022-02-18 19:06:01 -0500

    Added variables needed and an array of objects to SearchKeyword function.

--- Main.py
+++ Main.py
@@ -8,7 +8,7 @@
 #
 # Michael Ashby
 # Start:    2/14/2022
-# Updated:  2/14/2022
+# Updated:  2/18/2022
 
 Library = ["Pride and Prejudice", "Frankenstein", "Sherlock Holmes", "Alice in Wonderland", "The Great Gatsby", "The Yellow Wallpaper", "A Tale of Two Cities", "Moby Dick", "Ulysses", "Dracula"]
 
@@ -18,6 +18,7 @@ BookSelected = None
 KeywordFoundString = None
 userInputString = None
 mainMenuString = None
+mainMenuString = "Type the keyword you wish to search:" + "\n"
 
 class BookandPosition():
     def __init__(self, BookName, Position):
@@ -26,19 +27,27 @@ class BookandPosition():
 
 # Function to retrieve three lines for three different books from keyword selected based on the user input.
 def SearchKeyword(Keyword): 
+    BookTitle1 = None
+    BookTitle2 = None
+    BookTitle3 = None
+    PassageLine = None
+    PassageLine2 = None
+    PassageLine3 = None
+    PassagesFound = []
 
     for x in range(3):
+    # Need a function check each word parsed by spaces. Possibility to add partial keywords found, wildcards.
+        print("Success")
     
-PassagesFound = [BookandPosition(BookTitle1,PassageLine), BookandPosition(BookTitle2,PassageLine2)-
-# Return a 2D Array of objects (Title, Position) 
-return KeywordFoundString
+    # Inside or out of loop depending how I build the array on book string at a time.
+    PassagesFound = [BookandPosition(BookTitle1,PassageLine), BookandPosition(BookTitle2,PassageLine2), BookandPosition(BookTitle3,PassageLine3)]
+    return PassagesFound
 
 
-mainMenuString = input("Type the keyword you wish to search:" + "\n")
-SearchKeyword(mainMenuString)
+# Prompt user with main menu string, store input into string
+userInputString = input(mainMenuString)
 
-# Return the text opened to the first hit
-print("Here's the first result from your keyword: " + "\n" + KeywordFound) 
+# Print line label and call the SearchKeyword function. 
+print("Here's the first result from your keyword: " + "\n" + SearchKeyword(mainMenuString)) 
 
-# calling searchkeywprd function will return Warning if an invalid title was inputted.
-#   mainMenuString = input("Type the keyword you wish to search" + "\n"
+#Need functions to pull statistics like count, similar words found(Maybe adding widlcards for first and last letter.), and word association web graph.
