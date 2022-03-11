cec5a07c631e53dbcd28bf2421aade0bdf7290bd
Michael Ashby <msashby00@gmail.com>
2022-03-11 00:03:43 -0500

    Built BookInfo class to store data on the Books. Added books to directory to use Python's file open function.

--- Main.py
+++ Main.py
@@ -8,9 +8,9 @@
 #
 # Michael Ashby
 # Start:    2/14/2022
-# Updated:  2/18/2022
+# Updated:  3/10/2022
 
-Library = ["Pride and Prejudice", "Frankenstein", "Sherlock Holmes", "Alice in Wonderland", "The Great Gatsby", "The Yellow Wallpaper", "A Tale of Two Cities", "Moby Dick", "Ulysses", "Dracula"]
+Library = ["Pride and Prejudice.txt", "Frankenstein.txt", "The Adventures of Sherlock Holmes.txt", "Alice’s Adventures in Wonderland.txt", "The Great Gatsby.txt", "The Yellow Wallpaper.txt", "A TALE OF TWO CITIES.txt", "Moby-Dick.txt", "Ulysses.txt", "DRACULA.txt"]
 
 # Setting Strings to None to ease debugging and test cases.
 Keyword = None
@@ -20,20 +20,32 @@ UserInputString = None
 MainMenuString = "Type the keyword you wish to search:" + "\n"
 NumberOfBooks = 10
 
-class BookandPosition():
-    def __init__(self, BookName, Position):
-        self.BookName = BookName
-        self.Position = Position
+# Class to hold information on the book. 
+class BookInfo ():
+    Author = None
+    Title = None
+    Text = None
+    
+    def getAuthor(self):
+        return Author
+    def getTitle(self):
+        return Title
+    def getText(self):
+        return Text
+
 
 # Function to retrieve three lines for three different books from keyword selected based on the user input.
 def SearchKeyword(Keyword): 
-    PassagesFound = []
 
+    # Need an array of book objects
+    Books = []
+
+    # Need to use file open 
+    #Need to loop through the 10 books to store them in the book object then the book array.
     for x in range(NumberOfBooks):
-    # Need a function check each word parsed by spaces. Possibility to add partial keywords found, wildcards.
-        print("Ammend List")    
-    # Ill need a 2D list that ammends booktitle and the keyword's  index within the passed strings. Is every book going to be its own string? Maybe a List of book strings.
-    return print("Success")
+        print("")    
+    
+    return print("")
 
 
 # Prompt user with main menu string, store input into string
@@ -42,4 +54,4 @@ UserInputString = input(MainMenuString)
 # Print line label and call the SearchKeyword function. 
 print("Here's the first result from your keyword: " + "\n" + SearchKeyword(MainMenuString)) 
 
-#Need functions to pull statistics like count, similar words found(Maybe adding widlcards for first and last letter.), and word association web graph.
+# Need functions to pull statistics like count, similar words found(Maybe adding widlcards for first and last letter.), and word association web graph.
